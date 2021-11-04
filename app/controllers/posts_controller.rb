c6ff89b5cd12283451e46075e62ab9e582a28922
Liocha <dev@liocha.ru>
2021-11-04 07:19:24 +0300

    step 4

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,29 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new(post_params)
+    @post.creator_id = current_user.id
+    if @post.save
+      redirect_to root_url
+    else
+      render :new
+    end
+  end
+
+  private
+    def post_params
+      params[:post].permit!
+    end
+
+end
---
+++ app/models/post.rb
@@ -0,0 +1,3 @@
+class Post < ApplicationRecord
+  has_one :post_category  
+end
