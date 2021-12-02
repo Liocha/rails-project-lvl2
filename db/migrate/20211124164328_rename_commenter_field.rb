43e984d7fccf2d2b024d3f4901d7248ad0969815
Liocha <dev@liocha.ru>
2021-12-02 03:31:30 +0300

    lint fix

--- db/migrate/20211124164328_rename_commenter_field.rb
+++ db/migrate/20211124164328_rename_commenter_field.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# top-level documentation comment for class RenameCommenterField
 class RenameCommenterField < ActiveRecord::Migration[6.1]
   def change
     rename_column :post_comments, :commenter, :commenter_id
--- db/migrate/20211201233011_rename_creator_to_creator_id_in_post_like.rb
+++ db/migrate/20211201233011_rename_creator_to_creator_id_in_post_like.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Missing top-level documentation comment for class RenameCreatorToCreatorIdInPostLike
 class RenameCreatorToCreatorIdInPostLike < ActiveRecord::Migration[6.1]
   def change
     rename_column :post_likes, :creator, :creator_id
