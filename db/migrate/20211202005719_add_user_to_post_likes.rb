d353ab3f179a1644ac09d00c7703d40745cd815c
Liocha <dev@liocha.ru>
2021-12-02 05:02:47 +0300

    lint bug fix

--- db/migrate/20211202005719_add_user_to_post_likes.rb
+++ db/migrate/20211202005719_add_user_to_post_likes.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# top-level documentation comment for class AddUserToPostLikes
 class AddUserToPostLikes < ActiveRecord::Migration[6.1]
   def change
     add_foreign_key :post_likes, :users, column: :creator_id
