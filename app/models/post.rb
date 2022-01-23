76d075e82192cc6f9a2629f41630933f83070aab
Liocha <dev@liocha.ru>
2022-01-23 23:33:28 +0300

    7.1 removed unnecessary validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,5 +8,4 @@ class Post < ApplicationRecord
 
   validates :body, presence: true, length: { minimum: 50 }
   validates :title, presence: true, length: { minimum: 5 }
-  validates :post_category_id, presence: true
 end
