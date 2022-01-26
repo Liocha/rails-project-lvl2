dd12be4f0469199a6a44da8347c740e2965426ef
Liocha <dev@liocha.ru>
2022-01-26 03:32:18 +0300

    added restrictions in the router

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'posts#index'
-  resources :posts do
+  resources :posts, only: %i[index show new create] do
     resources :comments, only: [:create]
     resources :likes, only: %i[create destroy]
   end
