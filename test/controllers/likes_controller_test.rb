f4a78601930bd84903b271ddbcc6736451ac688b
Liocha <dev@liocha.ru>
2022-01-26 22:21:33 +0300

    LikesControllerTest remover crate test

--- test/controllers/likes_controller_test.rb
+++ test/controllers/likes_controller_test.rb
@@ -7,11 +7,4 @@ class LikesControllerTest < ActionDispatch::IntegrationTest
     @user = users(:one)
     @post = posts(:one)
   end
-
-#  test '#create' do
- #   sign_in @user
-  #  post post_likes_path(@post.id), params: {}
-  #  assert_equal(@post.likes.count, 1)
-  #  assert_response :redirect
- # end
 end
